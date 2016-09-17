2e5b68676fa7249a43c498d89b25f68df381cf54
Tuomas Pöyry <tuomas@munkkeli.io>
2016-09-17 18:11:31 +0300

    Added gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var less = require('gulp-less');
 var jade = require('gulp-jade');
 var cssnano = require('gulp-cssnano');
@@ -24,73 +25,73 @@ var banner = ['/**',
 var destination = './build';
 
 var paths = {
-	less: ['./assets/less/*.less'],
-	js: ['./assets/js/*.js'],
+  less: ['./assets/less/*.less'],
+  js: ['./assets/js/*.js'],
   views: ['./views/*.jade', '!./views/layout.jade'],
-	plugins: ['./assets/js/plugins/*.js'],
-	css: ['./assets/css/*.css'],
-	fonts: ['./assets/fonts/*.*'],
+  plugins: ['./assets/js/plugins/*.js'],
+  css: ['./assets/css/*.css'],
+  fonts: ['./assets/fonts/*.*'],
   images: ['./assets/images/*.*'],
   assets: ['./assets/*.*']
 };
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   return gulp.src(paths.less)
     .pipe(less())
     .pipe(cssnano())
-    .pipe(header(banner, { pkg : pkg }))
+    .pipe(header(banner, { pkg: pkg }))
     .pipe(rename(function (path) {
-	    path.extname = '.min.css';
-	  }))
+      path.extname = '.min.css';
+    }))
     .pipe(gulp.dest(destination + '/css'));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src(paths.js)
     .pipe(development(sourcemaps.init()))
-    .pipe(uglify())
-    .pipe(header(banner, { pkg : pkg }))
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(header(banner, { pkg: pkg }))
     .pipe(rename(function (path) {
-	    path.extname = '.min.js';
-	  }))
+      path.extname = '.min.js';
+    }))
     .pipe(development(sourcemaps.write('./')))
     .pipe(gulp.dest(destination + '/js'));
 });
 
-gulp.task('views', function() {
+gulp.task('views', function () {
   gulp.src(paths.views)
     .pipe(jade())
-    .pipe(gulp.dest(destination))
+    .pipe(gulp.dest(destination));
 });
 
-gulp.task('plugins', function() {
+gulp.task('plugins', function () {
   return gulp.src(paths.plugins)
-  	.pipe(concat('plugins.min.js'))
+    .pipe(concat('plugins.min.js'))
     .pipe(gulp.dest(destination + '/js'));
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp.src(paths.css)
-  	.pipe(concat('plugins.min.css'))
+    .pipe(concat('plugins.min.css'))
     .pipe(gulp.dest(destination + '/css'));
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
   return gulp.src(paths.fonts)
     .pipe(gulp.dest(destination + '/fonts'));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(paths.images)
     .pipe(gulp.dest(destination + '/images'));
 });
 
-gulp.task('assets', function() {
+gulp.task('assets', function () {
   return gulp.src(paths.assets)
     .pipe(gulp.dest(destination));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(paths.less, ['less']);
   gulp.watch(paths.js, ['js']);
   gulp.watch([paths.views, './views/layout.jade'], ['views']);
